43acc5c44e06b9207f49bbefd6ea738c1f91d8b5
anri-kbt <anrseys2@gmail.com>
2022-04-08 08:56:58 +0000

    {add}bookers2-8c

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,7 +9,8 @@ class GroupsController < ApplicationController
   end
 
   def show
-    @group=Group.find(params[:group_id])
+    @group=Group.find(params[:id])
+    @user=current_user
   end
 
   def new
@@ -19,22 +20,31 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     @group.owner_id = current_user.id
-    if @group.save!
+    @group.users << current_user
+    if @group.save
       redirect_to groups_path
     else
       render :new
     end
   end
 
+  def join
+    @group = Group.find(params[:group_id])
+    if !@group.users.include?(current_user)
+      @group.users << current_user
+      redirect_to groups_path
+    end
+  end
+
   def edit
-    @group=Group.find(params[:group_id])
-    if @group.user=current_user
+    @group = Group.find(params[:id])
+    if @group.owner_id=current_user.id
       render :edit
     end
   end
 
   def update
-    @group=Group.find(params[:group_id])
+    @group=Group.find(params[:id])
     if @group.update(group_params)
       redirect_to groups_path
     else
@@ -42,9 +52,22 @@ class GroupsController < ApplicationController
     end
   end
 
+  def destroy
+    @group = Group.find(params[:id])
+    @group.users.delete(current_user) #current_userは@group.usersから消される
+    redirect_to groups_path
+  end
+
   private
   def group_params
     params.require(:group).permit(:name, :introduction, :image)
+  end
+
+  def ensure_correct_user
+    @group = Group.find(params[:id])
+    unless @group.owner_id == current_user.id
+      redirect_to groups_path
+    end
   end
 
 end
--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,8 @@ Bundler.require(*Rails.groups)
 module Bookers2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+
+
 
     # Configuration for the application, engines, and railties goes here.
     #
