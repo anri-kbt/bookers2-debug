e718fe86cdda875f385df9bc9f18b07a78edca7b
anri-kbt <anrseys2@gmail.com>
2022-04-08 13:34:23 +0000

    {add}bookers2-debug9c

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -51,6 +51,18 @@ class GroupsController < ApplicationController
       render "edit"
     end
   end
+  
+  def new_mail
+    @group = Group.find(params[:group_id])
+  end
+  
+  def send_mail
+    @group = Group.find(params[:group_id])
+    group_users = @group.users
+    @mail_title = params[:mail_title]
+    @mail_content = params[:mail_content]
+    ContactMailer.send_mail(@gmail_title, @mail_content,group_users).deliver
+  
 
   def destroy
     @group = Group.find(params[:id])
